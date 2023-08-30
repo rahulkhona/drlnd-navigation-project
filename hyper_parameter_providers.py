33595dedcbd69bb1608a22c7682be86d70af32bb
rahulkhona <rkhona@gmail.com>
2023-08-30 12:49:59 -0700

    parameter provider and its tester

--- hyper_parameter_providers.py
+++ hyper_parameter_providers.py
@@ -5,57 +5,71 @@ class HyperParameterProvider:
     def __init__(self):
         self.value = None
         self.increment = 0
+
+    def next(self):
+        self.increment += 1
+        return self.increment
+
     def get(self)->float|int:
         raise NotImplemented("This is abstract class, use a concrete subclass")
 
-class ConstantParameterProvider:
+class ConstantParameterProvider(HyperParameterProvider):
     def __init__(self, value:int|float):
         super(ConstantParameterProvider, self).__init__()
         self.value = value
     def get(self)->float|int:
         self.increment += 1
         return self.value
+class LinearChangeParameterProvider(HyperParameterProvider):
+    def __init__(self, start:float, limit:float, slope:float):
+        super(LinearChangeParameterProvider, self).__init__()
+        self.start = start
+        self.limit = limit
+        self.rate = slope
+        if self.rate < 0:
+            if start < limit:
+                raise ValueError("For negative slopes, start must be larger than limi")
+        elif self.rate > 0:
+            if start > limit:
+                raise ValueError("For positive slopes, start must be smaller than limi")
+        elif self.rate == 0:
+            if start != limit:
+                raise ValueError("For 0 slope, start must be equal to limit")
+
+    def compute_value(self):
+        timestep = self.next()
+        return self.start + self.rate * timestep
+
+    def get(self)->float:
+        if np.isclose(self.start, self.limit):
+            return self.start
+        value = self.compute_value()
+        if self.rate < 0.0:
+            return max(self.limit, value)
+        else:
+            return min(self.limit, value)
+
+class ExponentialChangeParameterProvider(LinearChangeParameterProvider):
+    def __init__(self, start:float, limit:float, rate:float):
+        super(ExponentialChangeParameterProvider, self).__init__(start, limit, rate)
     
-class LinearAnealingParameterProvider(HyperParameterProvider):
-    def __init__(self, min:float=0, max:float=0):
-        super(LinearAnealingParameterProvider, self).__init__(max)
-        if min > max:
-            raise ValueError("Min should be smaller than max")
-        self.min = min
-        self.max = max
-    def get(self)->float:
-        self.increment += 1
-        return max(self.max / self.increment, self.min)
+    def compute_value(self):
+        timestep = self.next()
+        return self.start * np.exp(self.rate * timestep)
 
-class ExponentialDecayParameterProvider(LinearAnealingParameterProvider):
-    def __init__(self, min:float, max:float, decay_rate:float):
-        super(ExponentialDecayParameterProvider, self).__init__(self, min, max)
-        if decay_rate < 0 or decay_rate > 1:
-            raise ValueError("Decay Rate should be between 0 and 1")
-        self.decay_rate = decay_rate
+class ScheduledChangeParameterProvider(HyperParameterProvider):
+    def __init__(self, timesteps:[int], values:[float]):
+        super(ScheduledChangeParameterProvider, self).__init__()
+        if len(timesteps) != len(values):
+            raise ValueError("number of timesteps should be same as number of values")
+        self.q = []
+        for t,v in zip(timesteps, values):
+            self.q.append((t, v))
 
     def get(self)->float:
-        self.increment += 1
-        return max(self.max * np.exp(-self.decay_rate * self.increment), self.min)
-        #return max(max * (1 - self.decay_rate)**self.increment, min)
-
-class ExponentialGrowthParameterProvider(LinearAnealingParameterProvider):
-    def __init__(self, min:float, max:float, growth_rate:float):
-        super(ExponentialGrowthParameterProvider, self).__init__(self, min, max)
-        if growth_rate < 0 or growth_rate > 1:
-            raise ValueError("growth Rate should be between 0 and 1")
-        self.growth_rate = growth_rate
-    
-    def get(self)->float:
-        self.increment += 1
-        return min(self.max * np.exp(self.growth_rate * self.increment), self.min)
-        #return min((self.max + self.growth_rate))**self.increment, self.max)
-
-class LinearGrowthParameterProvider(LinearAnealingParameterProvider):
-    def __init__(self, min:float, max:float, slope:float):
-        super(LinearGrowthParameterProvider, self).__init__(self, min, max)
-        self.slope = slope
-
-    def get(self)->float:
-        self.increment += 1
-        return min(self.max, self.min + self.increment * self.slope)
+        timestep = self.next()
+        i = 0
+        while i < timestep and i < len(self.q):
+            v = self.q[i][1]
+            i+=1
+        return v
